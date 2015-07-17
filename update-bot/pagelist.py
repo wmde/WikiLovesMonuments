666bcae1bd281ec0ede2bce6d835d26aca926685
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-07-17 13:28:58 +0200

    Add docstrings to pagelist

--- update-bot/pagelist.py
+++ update-bot/pagelist.py
@@ -2,12 +2,14 @@ import pywikibot
 from pywikibot import config
 
 class Pagelist(object):
-
+    """ Utility class top get subcategories and articles for WLM 2015 """
     def __init__(self, site):
         self.site = site
 
     def get_county_categories(self):
+        """ Get all subcategories of Liste (Kulturdenkmale in Deutschland) """
         return pywikibot.Category(self.site, "Liste (Kulturdenkmale in Deutschland)").subcategories()
 
     def get_list_articles(self):
+        """ Return all the pages for all the categories"""
         return [article for cat in self.get_county_categories() for article in cat.articles()]
