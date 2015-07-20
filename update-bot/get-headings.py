30c852fe218f09228b5aad07085e6794f4fcb936
Andrew-WMDE <andrew.kostka@wikimedia.de>
2015-07-20 13:32:15 +0200

    added counter, added grouping, filtering out just names

--- update-bot/get-headings.py
+++ update-bot/get-headings.py
@@ -1,4 +1,7 @@
+__author__ = 'Andrew Pekarek-Kostka'
+
 import pywikibot
+from collections import Counter
 
 site = pywikibot.Site()
 page_names = [
@@ -7,7 +10,8 @@ page_names = [
                 'Liste der Kulturdenkmale in Wiegendorf',
                 'Liste der Kulturdenkmale in Wundersleben',
                 'Liste der Kulturdenkmale in Ostramondra',
-                'Liste der Kulturdenkmale in Tellingstedt'
+                'Liste der Kulturdenkmale in Tellingstedt',
+                'Liste der Baudenkmale in Neubrandenburg'
               ]
 
 
@@ -25,16 +29,18 @@ def main():
         progress += 1
         print('page: ' + str(progress))
 
-        search_start = '|- style='
+        search_start = '! '
         finder_start(page_content, location_start, search_start)
 
-        search_end = '\n|-'
+        search_end = '\n'
         finder_end(page_content, location_start, search_end, location_end)
 
         get_string(location_start, location_end, page_content, results)
 
-    for a in results:
-        print(a + '\n')
+    print('\nOriginal Array:')
+    print(results)
+    print('\nFrequency in Array:')
+    counter(results)
 
 
 def finder_start(page_content, location_start, search_start):
@@ -52,9 +58,29 @@ def finder_end(page_content, location_start, search_end, location_end):
 
 def get_string(location_start, location_end, page_content, results):
     x = 0
+    string = '|'
+
     while x < len(location_start):
-        results.append((page_content[location_start[x] + 36:location_end[x]]))
+        content = page_content[location_start[x]:location_end[x]]
+
+        try:
+            if location_start[x] - 1 == location_end[x - 1]:
+                string = string + content.split('|', 1)[-1] + '|'
+            elif location_start[x + 1] == location_end[x] + 1:
+                string = string + content.split('|', 1)[-1] + '|'
+            if location_start[x + 1] != location_end[x] + 1:
+                results.append(string)
+                string = '|'
+        except Exception:
+            pass
+
         x += 1
+
+
+def counter(results):
+    frequency = Counter(results)
+    print(frequency)
+
 
 if __name__ == '__main__':
     main()
