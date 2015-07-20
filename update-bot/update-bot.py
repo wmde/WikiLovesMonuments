e6fb8ce93d333f903896a7abcfc52c315f3d54e3
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-07-20 17:49:32 +0200

    Use logging library
    update-bopt now supports proper logging levels instead of using
    output_destination and verbose_output parameters.

--- update-bot/update-bot.py
+++ update-bot/update-bot.py
@@ -11,62 +11,69 @@ import sys
 import mwparserfromhell
 import random
 import operator
+import logging
 
 from template_replacer import TemplateReplacer
 # TODO: import TableReplacer
 
 WLM_PLACEHOLDER = '<-- link to commons placeholder #commonscat# -->' # TODO proper placeholder
 
-def add_placeholders(article, output_destination, verbose_output=False):
-    if verbose_output:
-        output_destination.write("{}\n".format(article.title()))
+def add_placeholders(article):
+    logging.info("{}".format(article.title()))
     if article.isRedirectPage():
         return
     text = article.get()
     # TODO check text from page for general commons category, if none is found, return with error msg
     commonscat = "Foo"
-    text_with_placeholders_in_templates = replace_in_templates(text, commonscat, output_destination, verbose_output)
-    text_with_placeholders_in_tables = replace_in_tables(text_with_placeholders_in_templates, commonscat, output_destination, verbose_output)
+    text_with_placeholders_in_templates = replace_in_templates(text, commonscat)
+    text_with_placeholders_in_tables = replace_in_tables(text_with_placeholders_in_templates, commonscat)
     if text != text_with_placeholders_in_tables:
         # TODO store new text
-        if verbose_output:
-            output_destination.write("Updated article with placeholders\n")
+        logging.info("  Updated article with placeholders")
+        logging.debug(text_with_placeholders_in_tables)
 
-def replace_in_templates(text, commonscat, output_destination, verbose_output=False):
+def replace_in_templates(text, commonscat):
     global WLM_PLACEHOLDER
     # fail fast
     if text.find("Tabellenzeile") == -1:
+        logging.info("   no templates found.")
         return text
     code = mwparserfromhell.parse(text)
     for template in code.filter_templates():
         if template.name.find("Tabellenzeile") == -1:
-            return
-        replacer = TemplateReplacer(t)
+            continue
+        replacer = TemplateReplacer(template)
         if replacer.param_is_empty("Bild"):
             # TODO check template for commonscat and assign it to row_commonscat
             row_commonscat = commonscat
             placeholder = WLM_PLACEHOLDER.replace("#commonscat#", row_commonscat)
-            if verbose_output:
-                output_destination.write("   ... inserting placeholder in {} \n".format(template))
+            logging.debug("   ... inserting placeholder in {}".format(template))
             replacer.set_value('Bild', placeholder)
             text = text.replace(unicode(template), unicode(replacer))
     return text
 
-def replace_in_tables(text, commonscat, output_destination, verbose_output=False):
+def replace_in_tables(text, commonscat):
     # TODO: Use table parser to replace table rows
     return text
 
 def main(*args):
     UTF8Writer = codecs.getwriter('utf8')
     output_destination = UTF8Writer(sys.stdout)
-    verbose = False
-
+    verbosity = logging.ERROR
+    for argument in pywikibot.handle_args(args):
+        if argument == "-v":
+            verbosity = logging.WARNING
+        elif argument == "-vv":
+            verbosity = logging.INFO
+        elif argument == "-vvv":
+            verbosity = logging.DEBUG
+    logging.basicConfig(level=verbosity, stream=output_destination)
     site = pywikibot.Site()
     limit = 100
     counter = 0
     # TODO use pagelist class and iterate over categories
     for article in pywikibot.Category(site, u"Liste_(Kulturdenkmale_in_Baden-Württemberg)").articles():
-        add_placeholders(article, output_destination, True)
+        add_placeholders(article)
         counter += 1
         if counter > limit:
             break
