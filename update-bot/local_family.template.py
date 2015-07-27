8cd965b4bac9cdecf2e296243df17d195df8546f
WMDE-Fisch <christoph.fischer@wikimedia.de>
2015-07-27 15:56:52 +0200

    removed spaces ( see PEP8 )

--- update-bot/local_family.template.py
+++ update-bot/local_family.template.py
@@ -5,20 +5,20 @@ from pywikibot import family
 
 # Local test wiki
 
-class Family( family.Family ):
-    def __init__( self ):
-        family.Family.__init__( self )
+class Family(family.Family):
+    def __init__(self):
+        family.Family.__init__(self)
         self.name = 'local'  # Set the family name; this should be the same as in the filename.
         self.langs = {
             'de': 'de.wikimedia.dev',  # Put the hostname here.
         }
 
 
-    def version( self, code ):
+    def version(self, code):
         return "1.25.1"  # The MediaWiki version used. Not very important in most cases.
 
 
-    def scriptpath( self, code ):
+    def scriptpath(self, code):
         return '/w'  # The relative path of index.php, api.php : look at your wiki address.
 
 # This line may need to be changed to /wiki or /w,
--- update-bot/wlmbots/update_bot.py
+++ update-bot/wlmbots/update_bot.py
@@ -19,74 +19,74 @@ from lib.template_replacer import TemplateReplacer
 WLM_PLACEHOLDER = '<-- link to commons placeholder "#commonscat#" -->'  # TODO proper placeholder
 
 
-def add_placeholders( article ):
-    logging.info( "{}".format( article.title( ) ) )
-    if article.isRedirectPage( ):
+def add_placeholders(article):
+    logging.info("{}".format(article.title()))
+    if article.isRedirectPage():
         return
-    text = article.get( )
-    commonscat = CommonscatMapper( ).get_commonscat_from_category_links( text )
+    text = article.get()
+    commonscat = CommonscatMapper().get_commonscat_from_category_links(text)
     if not commonscat:
-        logging.error( "  {} has no mapped category link.".format( article.title( ) ) )
+        logging.error("  {} has no mapped category link.".format(article.title()))
         return
-    text_with_placeholders_in_templates = replace_in_templates( text )
-    text_with_placeholders_in_tables = replace_in_tables( text_with_placeholders_in_templates, commonscat )
+    text_with_placeholders_in_templates = replace_in_templates(text)
+    text_with_placeholders_in_tables = replace_in_tables(text_with_placeholders_in_templates, commonscat)
     if text != text_with_placeholders_in_tables:
         # TODO store new text
-        logging.info( "  Updated article with placeholders" )
-        logging.debug( text_with_placeholders_in_tables )
+        logging.info("  Updated article with placeholders")
+        logging.debug(text_with_placeholders_in_tables)
 
 
-def replace_in_templates( text ):
+def replace_in_templates(text):
     global WLM_PLACEHOLDER
     # fail fast
-    if text.find( "Tabellenzeile" ) == -1:
-        logging.info( "   no templates found." )
+    if text.find("Tabellenzeile") == -1:
+        logging.info("   no templates found.")
         return text
-    mapper = CommonscatMapper( )
-    code = mwparserfromhell.parse( text )
-    for template in code.filter_templates( ):
-        if template.name.find( "Tabellenzeile" ) == -1:
+    mapper = CommonscatMapper()
+    code = mwparserfromhell.parse(text)
+    for template in code.filter_templates():
+        if template.name.find("Tabellenzeile") == -1:
             continue
-        replacer = TemplateReplacer( template )
-        if replacer.param_is_empty( "Bild" ):
-            row_commonscat = mapper.get_commonscat( text, template )
-            placeholder = WLM_PLACEHOLDER.replace( "#commonscat#", row_commonscat )
-            replacer.set_value( 'Bild', placeholder )
-            text = text.replace( unicode( template ), unicode( replacer ) )
+        replacer = TemplateReplacer(template)
+        if replacer.param_is_empty("Bild"):
+            row_commonscat = mapper.get_commonscat(text, template)
+            placeholder = WLM_PLACEHOLDER.replace("#commonscat#", row_commonscat)
+            replacer.set_value('Bild', placeholder)
+            text = text.replace(unicode(template), unicode(replacer))
     return text
 
 
-def replace_in_tables( text, commonscat ):
+def replace_in_tables(text, commonscat):
     # TODO Find table starts on page {|-
     # TODO parse table headings of each table to get a dictionary of heading => column_index
     # TODO find beginning of each table row, and use table_replacer to check/change image column of each row
     return text
 
 
-def main( *args ):
-    UTF8Writer = codecs.getwriter( 'utf8' )
-    output_destination = UTF8Writer( sys.stdout )
+def main(*args):
+    UTF8Writer = codecs.getwriter('utf8')
+    output_destination = UTF8Writer(sys.stdout)
     verbosity = logging.ERROR
     limit = 0
-    for argument in pywikibot.handle_args( args ):
+    for argument in pywikibot.handle_args(args):
         if argument == "-v":
             verbosity = logging.WARNING
         elif argument == "-vv":
             verbosity = logging.INFO
         elif argument == "-vvv":
             verbosity = logging.DEBUG
-        elif argument.find( "-limit=" ) == 0:
-            limit = int( argument[7:] )
-    logging.basicConfig( level = verbosity, stream = output_destination )
-    site = pywikibot.Site( )
+        elif argument.find("-limit=") == 0:
+            limit = int(argument[7:])
+    logging.basicConfig(level=verbosity, stream=output_destination)
+    site = pywikibot.Site()
     counter = 0
     # TODO use pagelist class and iterate over categories
-    for article in pywikibot.Category( site, u"Liste_(Kulturdenkmale_in_Baden-Württemberg)" ).articles( ):
-        add_placeholders( article )
+    for article in pywikibot.Category(site, u"Liste_(Kulturdenkmale_in_Baden-Württemberg)").articles():
+        add_placeholders(article)
         counter += 1
         if limit and counter > limit:
             break
 
 
 if __name__ == "__main__":
-    main( )
+    main()
