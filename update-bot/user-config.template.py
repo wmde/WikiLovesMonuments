e9c2743f90d63c633ee30c7fbbbece82aa69c1eb
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-07-27 13:16:29 +0200

    Make variable private
    Running the bot created the error message "WARNING: Configuration
    variable 'local_filepath' is defined but unknown." That was the reason
    for the duplicated string in the previous versions. Using an underscore
    to indicate a private variable avoids the warning.

--- update-bot/user-config.template.py
+++ update-bot/user-config.template.py
@@ -7,8 +7,8 @@ family = 'local'
 #usernames['wikipedia']['de'] = 'WLM Update Bot'
 usernames['local']['de'] = u'admin'
 
-local_filepath = os.path.join(os.path.dirname(os.path.realpath('__file__')), 'local_family.py')
+_local_filepath = os.path.join(os.path.dirname(os.path.realpath('__file__')), 'local_family.py')
 
 # For local testing
-family_files['local'] = local_filepath # register_family_file is buggy, we have to assign this again
-register_family_file('local', local_filepath)
+family_files['local'] = _local_filepath # register_family_file is buggy, we have to assign this again
+register_family_file('local', _local_filepath)
