4421e5b7852dce1216decef299a5f6ec418d814b
WMDE-Fisch <christoph.fischer@wikimedia.de>
2015-07-27 17:37:53 +0200

    have all attribute names defined in __init__ once https://stackoverflow.com/questions/19284857/instance-attribute-attribute-name-defined-outside-init

--- update-bot/wlmbots/lib/template_checker.py
+++ update-bot/wlmbots/lib/template_checker.py
@@ -11,6 +11,7 @@ class TemplateChecker(object):
             config = {}
         self.tpl_match_regex = None
         self.config = config
+        self._config = None
 
     def text_contains_templates(self, text):
         if not self.tpl_match_regex:
