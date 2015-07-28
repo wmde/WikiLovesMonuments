d70d48192987e461e82336266bf9321d0ba2d184
WMDE-Fisch <christoph.fischer@wikimedia.de>
2015-07-28 12:32:57 +0200

    renamed bot and fixed variable names

--- update-bot/wlmbots/lib/get_headings_bot.py
+++ update-bot/wlmbots/lib/get_headings_bot.py
@@ -5,7 +5,7 @@ import os
 import pywikibot
 import collections
 
-site = pywikibot.Site()
+SITE = pywikibot.Site()
 
 
 def main():
@@ -25,7 +25,7 @@ def parser(page_names, input_file):
     results = []
 
     for page in page_names:
-        page_content = pywikibot.Page(site, page.decode('utf-8')).text
+        page_content = pywikibot.Page(SITE, page.decode('utf-8')).text
         location_start = []
         location_end = []
 
@@ -60,21 +60,21 @@ def finder_end(page_content, location_start, search_end, location_end):
 
 
 def get_string(location_start, location_end, page_content, results):
-    x = 0
+    loc = 0
     string = '|'
 
-    while x < len(location_start):
-        content = page_content[location_start[x]:location_end[x]]
+    while loc < len(location_start):
+        content = page_content[location_start[loc]:location_end[loc]]
 
-        if x == len(location_start) - 1 or location_start[x + 1] == location_end[x] + 1:
+        if loc == len(location_start) - 1 or location_start[loc + 1] == location_end[loc] + 1:
             string = string + content.split('|', 1)[-1] + '|'
-        elif x == len(location_start) - 1 or location_start[x] - 1 == location_end[x - 1]:
+        elif loc == len(location_start) - 1 or location_start[loc] - 1 == location_end[loc - 1]:
             string = string + content.split('|', 1)[-1] + '|'
-        if x == len(location_start) - 1 or location_start[x + 1] != location_end[x] + 1:
+        if loc == len(location_start) - 1 or location_start[loc + 1] != location_end[loc] + 1:
             results.append(string)
             string = '|'
 
-        x += 1
+        loc += 1
 
 
 def counter(results):
@@ -83,8 +83,8 @@ def counter(results):
 
 def writing_file(frequency, file_name):
     with open('formats/' + file_name, 'w') as format_file:
-        for f, count in frequency:
-            format_file.write('{}: {}\n'.format(f.encode('utf-8'), count))
+        for step, count in frequency:
+            format_file.write('{}: {}\n'.format(step.encode('utf-8'), count))
 
 if __name__ == '__main__':
     main()
