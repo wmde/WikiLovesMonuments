f6eae33e5df1de26a1be07ca7f4d2d99a24d7015
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-08-03 10:59:53 +0200

    Add headline to config table
    Add a headline and explanation before outputting the config table.

--- update-bot/wlmbots/checker_bot.py
+++ update-bot/wlmbots/checker_bot.py
@@ -102,7 +102,8 @@ class CheckerBot(object):
 
     def generate_config_table(self):
         line_fmt = "|-\n|[[Vorlage:{}|{}]]\n|{}\n|{}\n"
-        text = '{| class="wikitable"\n|-\n!Vorlage!!Bezeichner ID!!Format ID\n'
+        text = "== Unterstützte Vorlagen ==\nDie Seiten wurden mit folgenden Vorlagen und Einstellungen geprüft:\n"
+        text += '{| class="wikitable"\n|-\n!Vorlage!!Bezeichner ID!!Format ID\n'
         for template_name, config in sorted(self.checker.config.items()):
             text += line_fmt.format(template_name, template_name, config["id"], config["id_check_description"])
         text += "|}\n\n"
