cd2e46fd04f23f0152962f2695ccd5f066b90d9e
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-08-10 13:11:45 +0200

    Add CampaignValidator
    The CampaignValidator class checks if a given campaign name exists.

---
+++ forward_script/src/CampaignValidator.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace Wikimedia\ForwardScript;
+
+use Mediawiki\Api\MediawikiApi;
+use Mediawiki\Api\SimpleRequest;
+
+/**
+ * Checks if a given campaign name exists.
+ *
+ * This should only be used on Wikis where the "Campaign" namespace exists and has the ID 460.
+ * It is intended for use with Wikimedia Commons.
+ *
+ * @package Wikimedia\ForwardScript
+ */
+class CampaignValidator
+{
+
+    const CAMPAIGN_NAMESPACE = 460;
+
+    /**
+     * @var MediawikiApi
+     */
+    protected $api;
+
+    function __construct(MediawikiApi $api)
+    {
+        $this->api = $api;
+    }
+
+    /**
+     * Check if the campaign exists and if it is in the "Campaign:" namespace.
+     * 
+     * @param string $campaignName
+     * @return bool
+     */
+    public function isValidCampaign($campaignName)
+    {
+        $response = $this->api->getRequest(new SimpleRequest("query", array(
+            'titles' => $campaignName,
+        )));
+        $firstPage = array_values($response["query"]["pages"])[0];
+        if ( empty($firstPage["ns"]) || $firstPage["ns"] != self::CAMPAIGN_NAMESPACE ) {
+            return false;
+        }
+        if ( isset($firstPage['pageid']) && !isset($firstPage["missing"])) {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ forward_script/tests/CampaignValidatorTest.php
@@ -0,0 +1,67 @@
+<?php
+
+
+class CampaignValidatorTest extends PHPUnit_Framework_TestCase {
+
+    public function testValidatorReturnsTrueIfPageExists()
+    {
+        $api = $this->getMockBuilder("Mediawiki\\Api\\MediawikiApi")
+            ->disableOriginalConstructor()
+            ->getMock();
+        $api->method("getRequest")->willReturn([
+            "query" => [
+                "pages" => [
+                    "27388743" => [
+                        "pageid" => 27388743,
+                        "ns"=> 460,
+                        "title" => "Campaign:wlm-de-by"
+                    ]
+                ]
+            ]
+        ]);
+        $validator = new \Wikimedia\ForwardScript\CampaignValidator($api);
+        $this->assertTrue($validator->isValidCampaign("Campaign:wlm-de-by"));
+    }
+
+    public function testValidatorReturnsFalseIfPageIsMissing()
+    {
+        $api = $this->getMockBuilder("Mediawiki\\Api\\MediawikiApi")
+            ->disableOriginalConstructor()
+            ->getMock();
+        $api->method("getRequest")->willReturn([
+            "query" => [
+                "pages" => [
+                    "-1" => [
+                        "ns"=> 460,
+                        "title" => "Campaign:foobar",
+                        "missing" => ""
+                    ]
+                ]
+            ]
+        ]);
+        $validator = new \Wikimedia\ForwardScript\CampaignValidator($api);
+        $this->assertFalse($validator->isValidCampaign("Campaign:foobar"));
+    }
+
+
+    public function testValidatorReturnsFalseIfPageIsNotInCampaignNamespace()
+    {
+        $api = $this->getMockBuilder("Mediawiki\\Api\\MediawikiApi")
+            ->disableOriginalConstructor()
+            ->getMock();
+        $api->method("getRequest")->willReturn([
+            "query" => [
+                "pages" => [
+                    "27388743" => [
+                        "pageid" => 27388743,
+                        "ns"=> 1,
+                        "title" => "wlm-de-by"
+                    ]
+                ]
+            ]
+        ]);
+        $validator = new \Wikimedia\ForwardScript\CampaignValidator($api);
+        $this->assertFalse($validator->isValidCampaign("wlm-de-by"));
+    }
+
+}
