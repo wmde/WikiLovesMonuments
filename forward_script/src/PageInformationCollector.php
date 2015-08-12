ba73ee62546f80406a3782334f3378c576feb492
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-08-12 12:54:03 +0200

    Improve PageInformationCollector
    Split up method, add docstrings to methods.
    This resolves
    https://github.com/wmde/WikiLovesMonuments/pull/58#discussion_r36845039

--- forward_script/src/PageInformationCollector.php
+++ forward_script/src/PageInformationCollector.php
@@ -36,24 +36,67 @@ class PageInformationCollector {
 		$this->defaultCategories = $defaultCategories;
 	}
 
+	/**
+	 * Get information about page categories and the monument ID.
+	 *
+	 * @param string $pageTitle
+	 * @param string $monumentId
+	 * @return object
+	 */
 	public function getInformation( $pageTitle, $monumentId ) {
+		list( $content, $categories ) = $this->getPageContentAndCategoriesFromAPI( $pageTitle );
+		$info = $this->getInformationFromProcess( $content, $monumentId );
+		if ( empty( $info->category ) ) {
+			$info->category = $this->determineCategory( $categories );
+		}
+		return $info;
+	}
+
+	/**
+	 * Query the MediaWiki API for page content and categories
+	 *
+	 * @param string $pageTitle
+	 * @return array
+	 */
+	protected function getPageContentAndCategoriesFromAPI( $pageTitle ) {
 		$response = $this->api->getRequest( new SimpleRequest( 'query', array(
 			'titles' => $pageTitle,
 			'prop' => 'categories|revisions',
 			'rvprop' => 'content'
 		) ) );
 		$firstPage = array_values( $response[ 'query' ][ 'pages' ] )[ 0 ];
-		if ( isset( $firstPage[ 'missing' ] ) ) {
-			throw new ApplicationException( "Page '$pageTitle' not found." );
-		}
-		if ( isset( $firstPage[ 'invalid' ] ) ) {
-			throw new ApplicationException( 'Page name is invalid.' );
-		}
+		$this->validatePage( $firstPage );
 		$firstRevision = $firstPage['revisions'][0];
 		if ( $firstRevision['contentformat'] !== 'text/x-wiki' ) {
 			throw new ApplicationException( 'Page is not a wiki text page.' );
 		}
-		$this->processCommand->setInput( $firstRevision['*'] );
+		return [$firstRevision['*'], $firstPage['categories']];
+	}
+
+	/**
+	 * Check if the API result for a page contains markers for missing or invalid pages.
+	 *
+	 * @param array $page
+	 */
+	protected function validatePage( $page ) {
+		if ( isset( $page[ 'missing' ] ) ) {
+			throw new ApplicationException( "Page {$page['title']} not found." );
+		}
+		if ( isset( $page[ 'invalid' ] ) ) {
+			throw new ApplicationException( 'Page name is invalid.' );
+		}
+	}
+
+	/**
+	 * Call the Python script with the monument ID and the page text to get information
+	 * about page categories and the monument ID.
+	 *
+	 * @param string $pageContent
+	 * @param string $monumentId
+	 * @return object
+	 */
+	protected function getInformationFromProcess( $pageContent, $monumentId ) {
+		$this->processCommand->setInput( $pageContent );
 		$monumentIdParam = ' ' . escapeshellarg( $monumentId );
 		$this->processCommand->setCommandLine(
 			$this->processCommand->getCommandLine() . $monumentIdParam
@@ -62,20 +105,22 @@ class PageInformationCollector {
 		if ( !$this->processCommand->isSuccessful() ) {
 			throw new RuntimeException( $this->processCommand->getErrorOutput() );
 		}
-
-		$info = json_decode( $this->processCommand->getOutput() );
-		$this->determineCategory( $info, $firstPage['categories'] );
-		return $info;
+		return json_decode( $this->processCommand->getOutput() );
 	}
 
-	protected function determineCategory( $info, $pageCategories ) {
-		if ( !empty( $info->category ) ) {
-			return;
-		}
+	/**
+	 * Map one of the configured default categories to the page category.
+	 *
+	 * This is the fallback method when the Python script doesn't return a category
+	 * (because there was no usable one in the page text).
+	 *
+	 * @param array $pageCategories
+	 * @return string
+	 */
+	protected function determineCategory( $pageCategories ) {
 		foreach ( $pageCategories as $categoryInfo ) {
 			if ( !empty( $this->defaultCategories[$categoryInfo['title']] ) ) {
-				$info->category = $this->defaultCategories[$categoryInfo['title']];
-				return;
+				return $this->defaultCategories[$categoryInfo['title']];
 			}
 		}
 		throw new ApplicationException( 'No valid category found for page.' );
