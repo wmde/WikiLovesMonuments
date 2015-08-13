24fc1c3553cf67ed923e49534fda1b1e76f6acb8
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-08-13 10:28:12 +0200

    Add spaces

--- forward_script/src/QueryBuilder.php
+++ forward_script/src/QueryBuilder.php
@@ -5,7 +5,7 @@ namespace Wikimedia\ForwardScript;
 
 class QueryBuilder {
 
-	public function getQuery( $info, $pageName, $id='', $coordinates=[] ) {
+	public function getQuery( $info, $pageName, $id = '', $coordinates = [] ) {
 		$category = preg_replace( '/^(:?Category|Kategorie):/', '', $info->category );
 		$query = array_merge( array_filter( $coordinates ), [ 'categories' => $category ] );
 		if ( $id && empty( $info->duplicate_ids ) && empty( $info->id_not_found ) ) {
@@ -14,7 +14,7 @@ class QueryBuilder {
 		if ( !empty( $info->valid_id ) ) {
 			$query['fields[]'] = $id;
 		}
-		return '&'.http_build_query( $query );
+		return '&' . http_build_query( $query );
 	}
 
 }
