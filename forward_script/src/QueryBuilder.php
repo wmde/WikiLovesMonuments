68f74944a065d9abd5ceff23b65e4552a4415975
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-08-17 11:01:23 +0200

    Add parameter for hidden categories
    QueryBuilder now supports an additional parameter to add
    categories that don't come from the wiki page.
    
    This is for T107041.

--- forward_script/src/QueryBuilder.php
+++ forward_script/src/QueryBuilder.php
@@ -12,10 +12,15 @@ class QueryBuilder {
 	 * @param string $pageName Wikipedia page name
 	 * @param string $id Unique monument id
 	 * @param array $coordinates Contains "lat" and "lon" keys (which may be empty)
+	 * @param array $additionalCategories Additional category names that will be added to the
+	 * 				category from page info
 	 * @return string
 	 */
-	public function getQuery( PageInformation $info, $pageName, $id = '', array $coordinates = [] ) {
-		$query = array_merge( array_filter( $coordinates ), [ 'categories' => $info->getCategory() ] );
+
+	public function getQuery( PageInformation $info, $pageName, $id = '', array $coordinates = [],
+							  array $additionalCategories = [] ) {
+		$categories = implode( "|",  array_merge( [$info->getCategory()], $additionalCategories ) );
+		$query = array_merge( array_filter( $coordinates ), [ 'categories' =>  $categories ] );
 		if ( $info->hasUsableId() ) {
 			$query['objref'] = implode( '|', ['de', $pageName, $id] );
 		}
