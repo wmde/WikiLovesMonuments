6614aa6215bedff526067f5888a5abc9ccfc3d8e
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-08-19 11:26:58 +0200

    Remove path prefix on tool labs
    On tool labs, the routes break because the reverse proxy adds a
    subdirectory path (the tool name). Probaly there is a better way to fix
    this (lighttpd config, Silex middleware), but directly manipulating
    $_SERVER works as a quick fix.

--- forward_script/web/index.php
+++ forward_script/web/index.php
@@ -1,5 +1,12 @@
 <?php
 
+// Remove prefix on tool labs to make our routes work behind the reverse proxy
+if ( !empty( $_SERVER['HTTP_X_ORIGINAL_URI'] ) ) {
+	foreach ( ['HTTP_X_ORIGINAL_URI', 'REQUEST_URI'] as $serverVar ) {
+		$_SERVER[$serverVar] = preg_replace( "!^/wlm-de-utils!", '', $_SERVER[$serverVar] );
+	}
+}
+
 $app = require __DIR__ . '/../app.php';
 
 $app->run();
