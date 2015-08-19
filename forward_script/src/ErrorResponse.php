1f6bfe35b963f59ac956c9c415b67e5891062140
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-08-19 13:37:31 +0200

    Change ErrorResponse headers
    The task https://phabricator.wikimedia.org/T103662 describes how tool
    labs web applications can avoid getting proxied on 500-Status codes.

--- forward_script/src/ErrorResponse.php
+++ forward_script/src/ErrorResponse.php
@@ -15,8 +15,9 @@ use Symfony\Component\HttpFoundation\Response;
  * This is a special response for errors that can be handled well and should
  * display an informative message to the user.
  *
- * It creates a 200 OK HTTP status code because otherwise the user would
+ * It creates a 'X-Wikimedia-Debug' header because otherwise the user would
  * see the default Tool Labs error page.
+ * See https://wikitech.wikimedia.org/wiki/Help:Tool_Labs/Web#Error_pages
  *
  * It uses text/plain content type to avoid the need for HTML escaping.
  *
@@ -26,10 +27,8 @@ class ErrorResponse extends Response {
 	public function __construct( $content = '', $status = 200, $headers = array() ) {
 		$headers = array_merge(
 			[
-				// The normal status code will be overwritten by the Silex handler,
-				// see http://silex.sensiolabs.org/doc/usage.html#error-handlers
-				'X-Status-Code' => Response::HTTP_OK,
-				'Content-Type' => 'text/plain'
+				'Content-Type' => 'text/plain',
+				' X-Wikimedia-Debug' => '1'
 			],
 			$headers
 		);
