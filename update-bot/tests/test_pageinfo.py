8c908127f7091d663c7cf5848b37ace63008074a
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-08-26 10:35:02 +0200

    Fix pageinfo unittests

--- update-bot/tests/test_pageinfo.py
+++ update-bot/tests/test_pageinfo.py
@@ -13,7 +13,7 @@ class TestPageinfo(unittest.TestCase):
         self.checker = Mock()
 
     def test_get_info_returns_not_found_if_id_does_not_match(self):
-        self.checker.is_allowed_template.return_value = True
+        self.checker.filter_allowed_templates = Mock(side_effect=lambda x: x)
         self.checker.get_id.return_value = "1"
         self.mapper.get_commonscat_list_from_links.return_value = ["Weblink Category"]
         text = "{{Denkmalliste Sachsen Tabellenzeile|ID=1}}"
@@ -21,21 +21,25 @@ class TestPageinfo(unittest.TestCase):
         self.assertEqual(info, {"id_not_found": True, "category": "Weblink Category"})
 
     def test_get_info_returns_not_found_if_template_is_not_configured(self):
-        self.checker.is_allowed_template.return_value = False
+        self.checker.filter_allowed_templates = Mock(side_effect=lambda x: x)
         self.mapper.get_commonscat_list_from_links.return_value = ["Weblink Category"]
         text = "{{Denkmalliste Sachsen Tabellenzeile|ID=1}}"
         info = pageinfo.get_template_info(self.checker, self.mapper, text, "123")
         self.assertEqual(info, {"id_not_found": True, "category": "Weblink Category"})
 
     def test_get_info_accepts_empty_id(self):
-        self.checker.is_allowed_template.return_value = False
+        self.checker.filter_allowed_templates = Mock(side_effect=lambda x: x)
         self.mapper.get_commonscat_list_from_links.return_value = ["Weblink Category"]
         text = "{{Denkmalliste Sachsen Tabellenzeile|ID=1}}"
         info = pageinfo.get_template_info(self.checker, self.mapper, text, "")
-        self.assertEqual(info, {"id_not_found": True, "category": "Weblink Category"})
+        self.assertEqual(info, {
+            "id_not_found": True,
+            "category": "Weblink Category",
+            'missing_monument_id': True
+        })
 
     def test_get_info_returns_info(self):
-        self.checker.is_allowed_template.return_value = True
+        self.checker.filter_allowed_templates = Mock(side_effect=lambda x: x)
         self.checker.get_id.return_value = "1"
         self.checker.has_valid_id.return_value = True
         self.mapper.get_commonscat_list_from_links.return_value = [u"Category:Cultural heritage ensembles in Saxony"]
@@ -49,7 +53,7 @@ class TestPageinfo(unittest.TestCase):
         })
 
     def test_get_info_returns_id_validity(self):
-        self.checker.is_allowed_template.return_value = True
+        self.checker.filter_allowed_templates = Mock(side_effect=lambda x: x)
         self.checker.get_id.return_value = "1"
         self.checker.has_valid_id.return_value = False
         self.mapper.get_commonscat_list_from_links.return_value = [u"Category:Cultural heritage ensembles in Saxony"]
@@ -63,7 +67,7 @@ class TestPageinfo(unittest.TestCase):
         })
 
     def test_get_info_returns_duplicate_id_info(self):
-        self.checker.is_allowed_template.return_value = True
+        self.checker.filter_allowed_templates = Mock(side_effect=lambda x: x)
         self.checker.get_id.return_value = "1"
         self.checker.has_valid_id.return_value = True
         self.mapper.get_commonscat_list_from_links.return_value = [u"Category:Cultural heritage ensembles in Saxony"]
