edc918ccf07906d3a199cfa76967936e985b777d
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-08-31 17:06:15 +0200

    Improve CampaignValidator
    pywikibot has a namespace parameter that can be used to get the correct
    page. Use that parameter instead of checking namespace param.

--- forward_script_python/forward_script/lib/campaign_validator.py
+++ forward_script_python/forward_script/lib/campaign_validator.py
@@ -2,19 +2,14 @@ class CampaignValidator(object):
 
     CAMPAIGN_NAMESPACE = 460
 
-    def __init__(self, pywikibot):
+    def __init__(self, pywikibot, site):
         self.pywikibot = pywikibot
+        self.site = site
 
     def is_valid_campaign(self, campaign_name):
-        page = self.pywikibot.Page(campaign_name)
-        return self._page_is_valid(page) and self._page_is_campaign(page)
+        page = self.pywikibot.Page(self. site, campaign_name, self.CAMPAIGN_NAMESPACE)
+        return self._page_is_valid(page)
 
     @staticmethod
     def _page_is_valid(page):
-        e = page.exists()
-        r = page.isRedirectPage()
-        return e and not r #page.exists() and not page.isRedirectPage()
-
-    def _page_is_campaign(self, page):
-        iscpn = page.namespace() == self.CAMPAIGN_NAMESPACE
-        return iscpn
+        return page.exists() and not page.isRedirectPage()
--- forward_script_python/tests/test_campaign_validator.py
+++ forward_script_python/tests/test_campaign_validator.py
@@ -10,7 +10,7 @@ class TestCampaignChecker(unittest.TestCase):
 
     def setUp(self):
         self.pywikibot = Mock()
-        self.checker = CampaignValidator(self.pywikibot)
+        self.checker = CampaignValidator(self.pywikibot, site=Mock())
 
     def test_page_must_exist_to_be_valid(self):
         page = Mock()
@@ -22,19 +22,10 @@ class TestCampaignChecker(unittest.TestCase):
         self.pywikibot.Page.return_value = page
         self.assertEqual(self.checker.is_valid_campaign("Test Campaign"), False)
 
-    def test_page_must_be_in_campaign_namespace(self):
-        page = Mock()
-        page.exists.return_value = True
-        page.isRedirectPage.return_value = False
-        page.namespace.return_value = 0
-        self.pywikibot.Page.return_value = page
-        self.assertEqual(self.checker.is_valid_campaign("Test Campaign"), False)
-
     def test_valid_campaign_page(self):
         page = Mock()
         page.exists.return_value = True
         page.isRedirectPage.return_value = False
-        page.namespace.return_value = 460
         self.pywikibot.Page.return_value = page
         self.assertTrue(self.checker.is_valid_campaign("Test Campaign"))
 
