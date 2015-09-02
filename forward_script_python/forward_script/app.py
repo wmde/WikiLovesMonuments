732b686e007e12e3789e71b7f75ca3aad1d474f1
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-09-02 17:55:29 +0200

    Implement forwarding
    The Flask app now does forwarding with the PageInformation object and
    query_builder.

--- forward_script_python/forward_script/app.py
+++ forward_script_python/forward_script/app.py
@@ -1,10 +1,15 @@
 import pywikibot
 
-from flask import Flask, request, session, g, redirect, render_template_string
+from flask import Flask, request, g, redirect, render_template_string
 
 import logging
 
 from lib.campaign_validator import CampaignValidator
+from lib.page_information import PageInformationCollector
+from lib import query_builder
+
+from wlmbots.lib.commonscat_mapper import CommonscatMapper
+from wlmbots.lib.template_checker import TemplateChecker
 
 
 # default configuration
@@ -25,8 +30,13 @@ app.logger.addHandler(file_handler)
 
 def setup_instances():
     g.site_commons = pywikibot.Site("commons", "commons")
-    g.site_wikipedia = pywikibot.Site("wikipedia", "de")
+    g.site_wikipedia = pywikibot.Site("de", "wikipedia")
     g.campaign_validator = CampaignValidator(pywikibot, g.site_commons)
+    checker = TemplateChecker()
+    checker.load_config("config/templates.json")
+    mapper = CommonscatMapper()
+    mapper.load_mapping("config/commonscat_mapping.json")
+    g.page_information_collector = PageInformationCollector(checker, mapper)
 
 
 @app.route('/')
@@ -39,7 +49,18 @@ def redirect_to_commons(page_name, campaign_name):
     setup_instances()
     if not g.campaign_validator.is_valid_campaign(campaign_name):
         return render_template_string("Invalid campaign_name.")
-    return redirect(app.config["COMMONS_UPLOAD_URL"] + "campaign="+campaign_name)
+    page_name = page_name.replace("+", "_")
+    article = pywikibot.Page(g.site_wikipedia, page_name)
+    monument_id = request.args.get('id', '')
+    page_information = g.page_information_collector.get_information(article, monument_id)
+    app.logger.debug(page_information.__dict__)
+    coordinates = {
+        "lat": request.args.get('lat', ''),
+        "lon": request.args.get('lon', ''),
+    }
+    query = query_builder.get_query(campaign_name, page_information, page_name, monument_id,
+                                    coordinates, app.config["ADDITIONAL_CATEGORIES"])
+    return redirect(app.config["COMMONS_UPLOAD_URL"] + query[1:])
 
 
 if __name__ == '__main__':
