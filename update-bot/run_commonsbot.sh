7534754e9292c9505479235e731eb1ffef3435ed
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-09-10 16:40:48 +0200

    Add usage example for bigger intervals
    Added a usage example on how to run commons bot with different intervals
    between updates.

--- update-bot/run_commonsbot.sh
+++ update-bot/run_commonsbot.sh
@@ -2,3 +2,8 @@
 # This script is a shortcut for running the commons bot on tools labs with all the parameters
 
 /data/project/wlm-de-utils/env/bin/python -m wlmbots.commons_bot -v
+
+# The following line could be used to run commons bot with an update freqency of 8 hours
+# /data/project/wlm-de-utils/env/bin/python -m wlmbots.commons_bot -v -sleep-seconds:28800
+# Every six hours would be 21600, every 4 hours 14400, every 3 hours 10800
+# All intervals are calculated from the time the bot starts
